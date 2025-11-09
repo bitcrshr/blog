b32987c3f81f9e9a4fc1d2ad8b3f8e9fddadf7ef
Chandler Davis <me@bitcrshr.dev>
2025-11-08 19:59:32 -0500

    update base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "blog.bitcrshr.dev",
+    baseUrl: "bitcrshr.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
